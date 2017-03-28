0229424ff69deba7d7bd3d59b93c236c83eaa758
Frank Schlesinger <fschlesinger@isdeblnnm275.iscout.local>
2017-03-28 17:29:04 +0200

    made test fail again

--- src/test/scala/MyHandlerSuite.scala
+++ src/test/scala/MyHandlerSuite.scala
@@ -8,7 +8,7 @@ class MyHandlerSuite extends FunSuite {
 
   test("Given name string shall be returned in greeting message") {
     def namestr = "foobar"
-    assert(new MyHandler().handler(new Name(namestr), null).message.contains(namestr), "Greeting does not contain " + namestr)
+    assert("Hello "+ namestr + ". Have a nice day!" === new MyHandler().handler(new Name(namestr), null).message, "Greeting does not contain " + namestr)
   }
 
 }
