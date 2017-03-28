b58d1a8306f0ce4fb2ac8df43fb6099c0639bcb8
Frank Schlesinger <fschlesinger@isdeblnnm275.iscout.local>
2017-03-28 17:13:35 +0200

    made a meaningful test

--- src/test/scala/MyHandlerSuite.scala
+++ src/test/scala/MyHandlerSuite.scala
@@ -1,3 +1,4 @@
+import example._
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -5,8 +6,9 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class MyHandlerSuite extends FunSuite {
 
-  test("foobar") {
-    assert(1 == 2, "test should run")
+  test("Given name string shall be returned in greeting message") {
+    def namestr = "foobar"
+    assert(new MyHandler().handler(new Name(namestr), null).message.contains(namestr), "Greeting does not contain " + namestr)
   }
 
 }
